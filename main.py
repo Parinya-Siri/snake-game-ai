9deb57a581505acd1ec90b5215236d7f938482ed
parinya-siri <parinya.sirikatitum@gmail.com>
2023-02-19 15:45:02 +0000

    newly update : adding ending and replayable screen, adding displaying high score and ending new high score

--- main.py
+++ main.py
@@ -32,49 +32,61 @@ clock = pygame.time.Clock()
 # Define the font for the score
 font = pygame.font.SysFont(None, 25)
 
-# Define the starting position of the snake
-snake_position = [100, 40]
-snake_body = [[100, 50], [90, 50], [80, 50]]
-
-# Define the starting position of the food
-food_position = [random.randint(0, grid_width-1) * grid_size,
-                 random.randint(0, grid_height-1) * grid_size]
-
-# Define the starting score
-score = 0
-
-# Define the direction of the snake
-direction = "RIGHT"
-
 # Define the function to display the score
 def display_score(score):
     score_text = font.render("Score: " + str(score), True, WHITE)
     screen.blit(score_text, [0, 0])
+def display_high_score(high_score):
+    score_text = font.render("Highest Score: " + str(high_score), True, RED)
+    screen.blit(score_text, [400, 0])
+def display_game_over(score,high_score):
+    game_over_text = font.render("GAME OVER : Your score is :" + str(score), True, WHITE)
+    screen.blit(game_over_text, [200, 200])    
+
+# Define Reset state
+def reset():
+    # Define the starting position of the snake
+    snake_position = [100, 40]
+    snake_body = [[100, 40], [80, 40], [60, 40]]
+    # Define the starting position of the food
+    food_position = [random.randint(0, grid_width-1) * grid_size,
+                     random.randint(0, grid_height-1) * grid_size]
+    # Define the starting score
+    score = 0
+    new_high_score = 0
+    # Define the direction of the snake
+    direction = "RIGHT"
+    return snake_position,snake_body,food_position,score,direction,new_high_score
+    
+# Create game over screen
+    # play_again_button = pygame.Rect(screen_width/2 - 75, screen_height/2 + 50, 150, 50)
+    # pygame.draw.rect(game_over_screen, (0, 255, 0), play_again_button)    
 
 # Define the main game loop
 async def gameLoop():
-    global direction
-    global snake_position
-    global snake_body
-    global food_position
-    global score
-    
+    high_score = 0
+    new_high_score = 0
+    snake_position,snake_body,food_position,score,direction,new_high_score = reset()    
+    game = False
     game_over = False
-    
-    while not game_over:
+    while not game:
         # Set the event listeners
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    direction = "LEFT"
+                    if direction != "RIGHT":
+                        direction = "LEFT"
                 elif event.key == pygame.K_RIGHT:
-                    direction = "RIGHT"
+                    if direction != "LEFT":
+                        direction = "RIGHT"
                 elif event.key == pygame.K_UP:
-                    direction = "UP"
+                    if direction != "DOWN":
+                        direction = "UP"
                 elif event.key == pygame.K_DOWN:
-                    direction = "DOWN"
+                    if direction != "UP":
+                        direction = "DOWN"
         
         # Update the position of the snake
         if direction == "RIGHT":
@@ -100,7 +112,7 @@ async def gameLoop():
         snake_head.append(snake_position[0])
         snake_head.append(snake_position[1])
         snake_body.insert(0, snake_head)
-        # print(snake_body)
+        
         # Check if the snake has eaten the food
         if snake_position == food_position:
             food_position = [random.randint(0, grid_width-1) * grid_size,
@@ -121,7 +133,11 @@ async def gameLoop():
         
         # Display the score
         display_score(score)
-        
+        # Display the highest score
+        if high_score<score:
+            high_score = score
+            new_high_score = 1
+        display_high_score(high_score)
         # Update the screen
         pygame.display.update()
         
@@ -129,7 +145,31 @@ async def gameLoop():
         clock.tick(10)
         
         await asyncio.sleep(0)
+        if game_over:
+            pause = True
+            game_over_screen = pygame.Surface((screen_width, screen_height))
+            game_over_screen.fill((0, 0, 0))
+            game_over_text = font.render("GAME OVER", True, RED)
+            if new_high_score == 1:
+                score_text = font.render("NEW HIGH SCORE! :" + str(score),True,WHITE)
+            else:
+                score_text = font.render("Youre score is :" + str(score),True,WHITE)
+            press_text = font.render("Press any key to replay", True, WHITE)
+            game_over_screen.blit(game_over_text, (screen_width/2 - game_over_text.get_width()/2, screen_height/2 - game_over_text.get_height()/2))
+            game_over_screen.blit(score_text, (screen_width/2 - score_text.get_width()/2, screen_height/2 - score_text.get_height()/2+50))
+            game_over_screen.blit(press_text, (screen_width/2 - press_text.get_width()/2, screen_height/2 - press_text.get_height()/2+100))
+            screen.blit(game_over_screen, (0, 0))
+            pygame.display.update()
+            while pause:
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        pause = False
+            # Reset the game state
+            snake_position,snake_body,food_position,score,direction,new_high_score = reset()  
+            game_over = False
+
 
 # Quit Pygame
 asyncio.run(gameLoop())
-pygame.quit()
+
+# pygame.quit()
